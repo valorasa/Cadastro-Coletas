b2a20b1f454f2b94ec6c023f689fd9861b85f3c6
EdjuniorM <edjunior.mota18@gmail.com>
2023-05-18 19:59:35 -0300

    erros de interface

--- src/pages/Destination/form.js
+++ src/pages/Destination/form.js
@@ -143,7 +143,7 @@ const Destination = () => {
             setDisardPlaceId("");
             setPickups([]);
 
-            await new Promise((resolve) => setTimeout(resolve, 1000));
+            await new Promise((resolve) => setTimeout(resolve, 500));
 
             navigate("./", { replace: true });
         } catch (error) {
