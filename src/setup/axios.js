12bd7d67650038a3fe972059f7f25bea2c00b201
EdjuniorM <edjunior.mota18@gmail.com>
2023-05-31 12:46:29 -0300

    api oficial

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
- // baseURL: "https://adm-api.valorareciclaveis.com.br/valora-api/backend/web/index.php/api",
-  baseURL: "https://valoraadm.devell.com.br/backend/web/index.php/api", // API de testes
-  timeout: 10000,
+  baseURL: "https://adm-api.valorareciclaveis.com.br/valora-api/backend/web/index.php/api",
+  timeout: 7000,
   headers: { Authorization: "Bearer RmvGoCB7yQiJw1x_al_JqlKPfabjm-SY" },
 });
 
 export default axiosInstance;
+
+
+ // baseURL: "https://valoraadm.devell.com.br/backend/web/index.php/api", // API de testes
