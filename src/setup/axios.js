52749434e292452e503a16054658aadf73b16b19
EdjuniorM <edjunior.mota18@gmail.com>
2023-06-19 08:23:49 -0300

    envs

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
- // baseURL: process.env.REACT_APP_BASE_URL,//"https://adm-api.valorareciclaveis.com.br/valora-api/backend/web/index.php/api",
-  baseURL: process.env.REACT_APP_BASE_URL_TESTE, //"https://valoraadm.devell.com.br/backend/web/index.php/api",
+  baseURL: process.env.REACT_APP_BASE_URL,//"https://adm-api.valorareciclaveis.com.br/valora-api/backend/web/index.php/api",
+ // baseURL: process.env.REACT_APP_BASE_URL_TESTE, //"https://valoraadm.devell.com.br/backend/web/index.php/api",
   timeout: 7000,
   headers: { Authorization: "Bearer RmvGoCB7yQiJw1x_al_JqlKPfabjm-SY" },
 });
