e7f7d96f8f05a600741511b0cf1e72a127875b2a
EdjuniorM <edjunior.mota18@gmail.com>
2023-06-23 17:25:35 -0300

    addSendMail

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
- // baseURL: process.env.REACT_APP_BASE_URL,//
-  baseURL: process.env.REACT_APP_BASE_URL_TESTE, //
+  baseURL: process.env.REACT_APP_BASE_URL,//
+  //baseURL: process.env.REACT_APP_BASE_URL_TESTE, //
   timeout: 7000,
   headers: { Authorization: "Bearer RmvGoCB7yQiJw1x_al_JqlKPfabjm-SY" },
 });
